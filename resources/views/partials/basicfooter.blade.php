75d2686b87dc813e06fcd950a075b778840c86df
OyanibTech-iii <pacificooyanib@gmail.com>
2026-06-16 23:25:23 +0800

    Implement code changes to enhance functionality and improve performance

---
+++ resources/views/partials/basicfooter.blade.php
@@ -0,0 +1,3 @@
+<div class="w-full max-w-2xl text-white bg-[#ffffff] dark:bg-[#1b1b18] p-6 lg:p-8 text-center">
+    <p class="text-sm">&copy; {{ date('Y') }} This is a basic footer for the page.</p>
+</div>
---
+++ resources/views/partials/samplefile.blade.php
@@ -0,0 +1,4 @@
+<div class="w-full max-w-2xl rounded-lg text-white bg-[#ffffff] dark:bg-[#1b1b18] p-6 lg:p-8 text-center">
+    <h1 class="text-lg text-red-600 ">Sample File in partial</h1>
+    <p class="text-sm">This is a sample file to test the layout and styling of the content.</p>
+</div>
