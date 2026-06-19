75b55377ec733dcf6947f5f5acd609900329f3b6
OyanibTech-iii <pacificooyanib@gmail.com>
2026-06-19 10:29:17 +0800

    Add admin dashboard route and view

---
+++ resources/views/admin_dashboard.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Admin Dashboard</title>
+</head>
+<body>
+    <div class="flex flex-col items-center justify-center min-h-screen">
+        <h1>Admin Dashboard</h1>
+        <p>Welcome, Admin!</p>
+     </div>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/admin_dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('admin_dashboard');
